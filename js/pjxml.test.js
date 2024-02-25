512010ed85ccda09222c7bfa1a8ccdef9dee56e3
Scott Means <smeans@gmail.com>
2024-02-25 06:25:34 +0000

    implemented bun/Jest tests

---
+++ js/pjxml.test.js
@@ -0,0 +1,11 @@
+import {expect, test} from "bun:test";
+const fs = require('fs');
+const pjxml = require('./pjxml.js');
+
+test("parse()", () => {
+	const xml = fs.readFileSync('test.xml', 'utf-8');
+	const doc = pjxml.parse(xml);
+	expect(doc).toMatchSnapshot();
+	expect(doc.xml()).toMatchSnapshot();
+	expect(doc.clone().xml()).toBe(doc.xml());
+});
